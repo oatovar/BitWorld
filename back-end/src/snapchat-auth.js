2d12478cbe96ebb33cd5c401b38ceac12c6c1c7d
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2019-04-13 23:37:47 -0500

    Updated snapchat authentication

--- back-end/src/snapchat-auth.js
+++ back-end/src/snapchat-auth.js
@@ -7,17 +7,25 @@ const buildURL = require('./helpers/snapchat/url-builder');
 /* Constants */
 const clientId = process.env.SNAP_CLIENT_ID;
 const clientSecret = process.env.SNAP_CLIENT_SECRET;
-const redirectURL = 'https://my-redirect-uir.com';
-const scopeList = ['https://auth.snapchat.com/oauth2/api/example.abc', 'https://auth.snapchat.com/oauth2/api/example.xyz'];
+const redirectURL = 'http://localhost:3030/success';
+const scopeList = ['https://auth.snapchat.com/oauth2/api/user.display_name', 'https://auth.snapchat.com/oauth2/api/user.bitmoji.avatar'];
 
 module.exports = function (app) {
-  app.get('/auth/sc', function(req, res, next) {
-    // Generate state
+  app.get('/snapchat', function(req, res, next) {
+    // Create 'state' token
     let state = generateClientState();
 
     // Build URL
     let getRedirectURL = buildURL(clientId, redirectURL, scopeList, state);
 
-    res.send(getRedirectURL);
+    res.redirect(getRedirectURL);
+  });
+
+  app.get('/success', function(req, res, next) {
+    /* TODO: add state tokens into a memory store such as rocksdb or redis
+     * and make sure that they are valid.
+     */
+    
+    res.send('Success!');
   });
 };
