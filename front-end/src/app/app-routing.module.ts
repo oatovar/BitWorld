c0609ca25bd11c8c1784bf2495c4d5798d2238e3
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2019-04-14 05:41:28 -0500

    Adding app routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { BitFeedComponent } from './bit-feed/bit-feed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'feed', component: BitFeedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
